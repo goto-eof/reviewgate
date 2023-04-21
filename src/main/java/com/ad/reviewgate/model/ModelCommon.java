da4dc90d2383113f80e8c9e10d3b64c43ac315e0
Andrei Dodu <andrei.dodu@hotmail.it>
2023-04-22 01:55:14 +0200

    feature: review model

---
+++ src/main/java/com/ad/reviewgate/model/ModelCommon.java
@@ -0,0 +1,51 @@
+package com.ad.reviewgate.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class ModelCommon {
+
+        @JsonIgnore
+        @CreatedDate
+        @Column(name = "insert_date", updatable = false, insertable = false)
+        protected LocalDateTime createdDate;
+
+        @JsonIgnore
+        @LastModifiedDate
+        @Column(name = "update_date", updatable = true, insertable = false)
+        protected LocalDateTime lastModifiedDate;
+
+        @JsonIgnore
+        @Column(name = "active")
+        protected Boolean active = Boolean.TRUE;
+
+        public LocalDateTime getCreatedDate() {
+                return createdDate;
+        }
+
+        public void setCreatedDate(LocalDateTime createdDate) {
+                this.createdDate = createdDate;
+        }
+
+        public LocalDateTime getLastModifiedDate() {
+                return lastModifiedDate;
+        }
+
+        public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+                this.lastModifiedDate = lastModifiedDate;
+        }
+
+        public Boolean getActive() {
+                return active;
+        }
+
+        public void setActive(Boolean active) {
+                this.active = active;
+        }
+}
--- src/main/java/com/ad/reviewgate/model/Review.java
+++ src/main/java/com/ad/reviewgate/model/Review.java
@@ -1,4 +1,53 @@
 package com.ad.reviewgate.model;
 
-public class Review {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "rg_review")
+public class Review extends ModelCommon {
+
+    @Id
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "rating", nullable = false)
+    private Integer rating;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
 }
